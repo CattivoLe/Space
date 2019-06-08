abe47abdf90445a64c61a0546f5c939e739c6600
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-06-08 15:13:59 +0300

    Background

--- Space/GameScene.swift
+++ Space/GameScene.swift
@@ -12,16 +12,31 @@ import GameplayKit
 class GameScene: SKScene {
     
     var spaceShip: SKSpriteNode!
+    var spaceBackground: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        spaceShip = SKSpriteNode(imageNamed: "spaceship")
-        addChild(spaceShip)
+        createNodes()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         moveSpaceSchip(touches, ship: spaceShip)
     }
     
+    //MARK: - Создаем елементы
+    final func createNodes() {
+        let screenSize = UIScreen.main.bounds
+        
+        spaceShip = SKSpriteNode(imageNamed: "spaceship")
+        spaceBackground = SKSpriteNode(imageNamed: "background")
+        
+        spaceBackground.size = CGSize(width: screenSize.width * 2.5, height: screenSize.height * 2.5)
+        spaceBackground.zPosition = -1
+        
+        addChild(spaceBackground)
+        addChild(spaceShip)
+    }
+    
+    //MARK: - Создаем движение
     final func moveSpaceSchip(_ touches: Set<UITouch>, ship: SKSpriteNode) {
         
         guard let touch = touches.first else {return}
