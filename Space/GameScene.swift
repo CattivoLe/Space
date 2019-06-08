62b834c4b80142ea2899ab84d7a7cd119df7fd59
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-06-08 14:48:25 +0300

    Move Node

--- Space/GameScene.swift
+++ Space/GameScene.swift
@@ -11,10 +11,25 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var spaceShip: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
-        let spaceShip = SKSpriteNode(imageNamed: "spaceship")
-        
+        spaceShip = SKSpriteNode(imageNamed: "spaceship")
         addChild(spaceShip)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        moveSpaceSchip(touches, ship: spaceShip)
+    }
+    
+    final func moveSpaceSchip(_ touches: Set<UITouch>, ship: SKSpriteNode) {
+        
+        guard let touch = touches.first else {return}
+        
+        let touchLocation = touch.location(in: self)
+        let moveAction = SKAction.move(to: touchLocation, duration: 0.5)
+        
+        ship.run(moveAction)
     }
     
     
